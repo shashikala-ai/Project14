90eb5fa103cf937269833c28738096f2a7d9f911
shashikala-ai <shashisowmya30@gmail.com>
2022-11-10 16:01:44 +0530

    commit

--- Project14/Source.cpp
+++ Project14/Source.cpp
@@ -39,7 +39,7 @@ public:
         cr.str1[1] = " Unit";
         cr.str2[1] = " Unit";
         cr.str3[1] = " Unit\n";
-        cr.str4[2] = " Unitiit\n";
+        cr.str4[2] = " Unitit\n";
         //Cylindrical
         cc.str1[0] = "Rho = ";
         cc.str2[0] = "Phi = ";
